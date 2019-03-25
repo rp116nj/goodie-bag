cbf65b79e934466dc12e291b3d15e6536a9b8e08
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-03-25 10:24:20 -0400

    commit the route

---
+++ server/api/candies.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const Candy = require('../db/models/Candy')
+router.get('/', async (req, res, next) => {
+  try {
+    const allCandies = await Candy.findAll()
+    res.json(allCandies)
+  }
+  catch (err) {
+    next(err)
+  }
+})
+module.exports = router;
--- server/api/index.js
+++ server/api/index.js
@@ -13,7 +13,7 @@ const router = require('express').Router()
 //
 // And for your `/api/kittens` routes:
 // router.use('/kittens', require('./kittens'))
-
+router.use('/candies', require('./candies'))
 // If someone makes a request that starts with `/api`,
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
